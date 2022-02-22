50ab9bccbd9c7523b974a55ba9dceee837825ae6
Shilpa Sanjay Jagtap <68769510+shilpa482@users.noreply.github.com>
2022-02-22 09:33:14 +0530

    new files added

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+            <div class="hk-footer-wrap container">
+                <footer class="footer">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+<h7>DBMS MINI_PROJECT</h7>
+                        </div>
+                    </div>
+                </footer>
+            </div>
